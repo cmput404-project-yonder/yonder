c2844f770aa71ea8f79268c2967a945717a91cbf
Anas Mohamed <amohamed@ualberta.ca>
2021-02-21 18:14:00 -0700

    Fixed Textarea not working in Post form

--- frontend/src/components/stream/posts/PostForm.jsx
+++ frontend/src/components/stream/posts/PostForm.jsx
@@ -3,8 +3,6 @@ import ReactMde from "react-mde";
 import { Form, Button, Panel, Card, Heading } from "react-bulma-components";
 import Markdown from "react-markdown";
 
-import { Post } from "../../../models/index";
-
 class PostForm extends Component {
   constructor(props) {
     super(props);
@@ -13,7 +11,13 @@ class PostForm extends Component {
 
     this.state = {
       posts: [],
-      newPost: Post,
+      title: "",
+      content: "",
+      contentType: "",
+      description: "",
+      unlisted: false,
+      visiblity: "PUBLIC",
+      categories: [],
       selectedTab: "text",
       markdownTab: "write",
     };
@@ -42,12 +46,11 @@ class PostForm extends Component {
     const textEditor = () => {
       return (
         <Form.Textarea
-          id="textBody"
-          value={this.state.newPost.content}
+          name="content"
+          value={this.state.content}
           onChange={this.onChange}
           style={{
             height: `245px`,
-            padding: `20px`,
           }}
         />
       );
@@ -56,8 +59,8 @@ class PostForm extends Component {
     const markdownEditor = () => {
       return (
         <ReactMde
-          value={this.state.newPost.content}
-          onChange={(m) => this.setState({ newPost: { content: m } })}
+          value={this.state.content}
+          onChange={(m) => this.setState({ content: m })}
           selectedTab={this.state.markdownTab}
           onTabChange={(t) => this.setState({ markdownTab: t })}
           generateMarkdownPreview={(markdown) => Promise.resolve(converter(markdown))}
@@ -71,29 +74,31 @@ class PostForm extends Component {
     };
 
     return (
-      <Card style={{ borderRadius: "10px", maxWidth: "640px" }}>
-        <Form.Field style={{ margin: "1em" }}>
+      <Card style={{ borderRadius: "10px", width: "480px" }}>
+        <Form.Field style={{ margin: "1em", padding: "10px" }}>
           <Heading size={4}>Create a Post</Heading>
           <Form.Label>Title:</Form.Label>
-          <Form.Textarea
-            onKeyPress={(e) => {
-              if (e.key === "Enter") e.preventDefault();
-            }}
-            maxLength="30"
-            cols={1}
-            id="textHeader"
-            value={this.state.newPost.title}
-            style={{
-              overflowY: "hidden",
-              whiteSpace: "nowrap",
-              resize: "none",
-              minWidth: "30vw",
-              height: `50px`,
-              padding: `10px`,
-            }}
-            onChange={this.onChange}
-          />
-
+          <Form.Control>
+            <Form.Textarea
+              onKeyPress={(e) => {
+                if (e.key === "Enter") e.preventDefault();
+              }}
+              maxLength="30"
+              cols={1}
+              name="title"
+              value={this.state.title}
+              style={{
+                overflowY: "hidden",
+                whiteSpace: "nowrap",
+                resize: "none",
+                height: `50px`,
+                padding: `10px`,
+              }}
+              onChange={this.onChange}
+            />
+          </Form.Control>
+        </Form.Field>
+        <Form.Field style={{ margin: "1em", padding: "10px" }}>
           <Form.Label>Content:</Form.Label>
           <Panel className="post-editor">
             <Panel.Tabs style={{ marginBottom: `-1em` }}>
@@ -108,9 +113,9 @@ class PostForm extends Component {
               </Panel.Tabs.Tab>
             </Panel.Tabs>
           </Panel>
-          {this.state.selectedTab === "text" ? textEditor() : null}
-          {this.state.selectedTab === "markdown" ? markdownEditor() : null}
           <Form.Control style={{ textAlign: "right" }}>
+            {this.state.selectedTab === "text" ? textEditor() : null}
+            {this.state.selectedTab === "markdown" ? markdownEditor() : null}
             <Button color="danger" onClick={() => this.props.setModalIsOpen(false)}>
               Cancel
             </Button>
