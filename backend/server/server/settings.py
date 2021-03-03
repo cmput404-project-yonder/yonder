9b9ecb2fa88802b536bbae9fae7c7f66464b0cc5
Anas Mohamed <amohamed@ualberta.ca>
2021-03-02 18:05:29 -0700

    Fixed environ misuse. Popeyes?

--- backend/server/server/settings.py
+++ backend/server/server/settings.py
@@ -111,7 +111,7 @@ WSGI_APPLICATION = 'server.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 
-if environ.get('CI'):
+if env('CI'):
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
