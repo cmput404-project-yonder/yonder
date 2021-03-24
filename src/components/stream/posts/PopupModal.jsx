636b474d2ad1e913e766dc5fb5a11b6196e7f7d9
gengyuan <gengyuan@ualberta.ca>
2021-03-23 19:07:04 -0600

    finish stream page UI

--- src/components/stream/posts/PopupModal.jsx
+++ src/components/stream/posts/PopupModal.jsx
@@ -4,14 +4,21 @@ import { Modal, Button } from "react-bulma-components";
 import PostForm from "./PostForm";
 import "./modal.css";
 
+import AddButton from "./NewPostFloatingButton";
+
+var buttonStyle = {
+  height: "4em",
+  width: "4em",
+  borderRadius: "100%",
+  boxShadow: "0pt 0pt 8pt #CCCCCC",
+  backgroundColor: "red"
+}
 
 function PopupModal(props) {
   const [modalIsOpen, setModalIsOpen] = useState(false);
   return (
     <div className="post-form-modal">
-      <Button color="primary" onClick={() => setModalIsOpen(true)}>
-        Create a Post
-      </Button>
+      <AddButton action={() => setModalIsOpen(true)}/>
       <Modal show={modalIsOpen} onClose={() => setModalIsOpen(false)} closeOnBlur closeOnEsc>
         <PostForm setModalIsOpen={setModalIsOpen} createPost={props.createPost} />
       </Modal>
