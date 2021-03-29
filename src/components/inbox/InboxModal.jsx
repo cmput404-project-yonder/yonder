3ec5482426ffc4b9c9383af0f6dd4a7c2b1877c7
gengyuan <gengyuan@ualberta.ca>
2021-03-29 03:40:07 -0600

    add inbox modal

--- src/components/inbox/InboxModal.jsx
+++ src/components/inbox/InboxModal.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import { Card } from "react-bulma-components";
-
+import {color} from "./styling";
+const inboxPageStyle = {
+    borderRadius: "8pt",
+    width: "60%",
+    height: "80%",
+    boxShadow: "0pt 0pt 8pt rgb(0,0,0,0.5)",
+    backgroundColor: color.backgroundCream,
+}
 
 class InboxModal extends React.Component {
     render() {
         return (
-            <Card>
+            <Card style={inboxPageStyle} >
             </Card>
         )
     }
---
+++ src/components/inbox/styling.jsx
@@ -0,0 +1,18 @@
+var color = {
+    baseBlack: "#505050",
+    baseWhite: "white",
+    baseRed: "#D18D8D",
+    baseLightGrey: "#AAAAAA",
+    buttonGreen:"#99D4B4",
+    buttonRed: "#DD7272",
+    buttonOrange: "#F68444",
+    postIcon: "#8fc9eb",
+    backgroundGrey: "#fefefe",
+    backgroundCream: "#F9F5EF",
+    backgroundCreamLighter: "#FFFAF1",
+}
+
+var font = {
+    segoeUI: "Segoe UI,Frutiger,Frutiger Linotype,Dejavu Sans,Helvetica Neue,Arial,sans-serif",
+}
+export {color, font};
