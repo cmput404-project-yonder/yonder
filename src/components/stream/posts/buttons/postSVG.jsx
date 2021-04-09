5c8c73b72869e8ca240f6aeda5f8e075d2b51f73
JamieJiHeonKim <jamiejiheonkim@gmail.com>
2021-04-09 15:52:08 -0600

    Updated ColorFontConfig import route

--- src/components/stream/posts/buttons/postSVG.jsx
+++ src/components/stream/posts/buttons/postSVG.jsx
@@ -1,5 +1,5 @@
 import { motion } from 'framer-motion';
-import { color } from '../../../styling/ColorFontConfig';
+import { color } from '../../../../styling/ColorFontConfig';
 /*
 SVG components:
     cancelButton
