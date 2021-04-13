e5c10afe811828540666ca24921ccc37e6b6039e
Anas Mohamed <amohamed@ualberta.ca>
2021-04-12 19:36:51 -0600

    removed post from request in create comment

--- backend/yonder/views.py
+++ backend/yonder/views.py
@@ -264,6 +264,8 @@ class comments(generics.ListCreateAPIView):
             node = get_object_or_404(RemoteNode, host=host)
             url = node.host + "api/author/" + request.data["post"]["author"]["id"] + \
                 "/posts/" + request.data["post"]["id"] + "/comments/"
+            del request.data["post"]
+
             response = requests.post(url, 
                 auth=requests.models.HTTPBasicAuth(node.our_user, node.our_password), 
                 json=request.data,
